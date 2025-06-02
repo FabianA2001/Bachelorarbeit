04823a6cddebe4eac8e0664c29e7f81d11994a39
“Fabian <farbkecks@gmx.de>
2025-06-02 12:52:22 +0200

    nix

--- code/src/main.py
+++ code/src/main.py
@@ -42,11 +42,12 @@ def custom_points() -> list[Node]:
 def test_algo():
     nodes = load_nodes_from_json("simple_40/000_delaunay_flips.json")
     # nodes = custom_points()
-    graph = Graph_Wrapper(nodes)
-    solver = SAT(graph)
-    solver.solve(timeout=10)
-    logging.info(f"evaluation: {graph.evaluate_graph()}")
-    graph.show_and_save()
+    for solver in [Raw_Flips, Random_Adder, Cycle_Add, Delaunay, Iterative]:
+        graph = Graph_Wrapper(nodes)
+        solver = solver(graph)
+        solver.solve(timeout=10)
+        # logging.info(f"evaluation: {graph.evaluate_graph()}")
+        graph.show_and_save()
 
 
 def move():
--- code/test.py
+++
@@ -1,5 +0,0 @@
-sollGrad = 7
-echterGrad = 7
-# x = echterGrad / sollGrad
-x = (sollGrad - abs(sollGrad - echterGrad)) / sollGrad
-print(x)
