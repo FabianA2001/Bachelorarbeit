2dec26eed1059ecece6fc04d80e341c180761226
Fabian A <>
2025-06-23 18:05:17 +0200

    format

--- code/evaluation/read_benchmark.py
+++ code/evaluation/read_benchmark.py
@@ -10,7 +10,7 @@ BENCHMARK_PATH = "./benchmark"
 if False:
     describe(BENCHMARK_PATH)
 else:
-    table = read_as_pandas(
+    df = read_as_pandas(
         BENCHMARK_PATH,
         lambda result: {
             "host": result["env"]["hostname"],
@@ -18,9 +18,7 @@ else:
             "instance": result["parameters"]["args"]["instance_name"],
             "file": result["parameters"]["args"]["file_name"],
             "correct": result["result"]["correct"],
-            "args": format_dictionary(
-                result["parameters"]["args"]["parameter"]["args"]
-            ),
+            "args": result["parameters"]["args"]["parameter"]["args"],
             "evaluation": result["result"]["evaluation"],
             "logging": result["logging"],
             "whole_runtime": result["runtime"],
@@ -30,4 +28,10 @@ else:
         },
     )
 
-    st.dataframe(table)
+    df["args"] = df["args"].apply(lambda x: format_dictionary(x))
+    df["logging"] = df["logging"].apply(lambda x: str(x))
+
+    st.dataframe(df)
+    # # Als HTML-Tabelle rendern
+    # html = df.to_html(escape=False)
+    # st.markdown(html, unsafe_allow_html=True)
