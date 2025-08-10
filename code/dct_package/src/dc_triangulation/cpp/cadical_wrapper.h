3c20e0a20d4da9b18834569afab09413e1bb0185
“Fabian <farbkecks@gmx.de>
2025-08-10 18:03:58 +0200

    change kernel

--- code/dct_package/src/dc_triangulation/cpp/cadical_wrapper.h
+++ code/dct_package/src/dc_triangulation/cpp/cadical_wrapper.h
@@ -12,10 +12,18 @@
 #include <CGAL/Arrangement_2.h>
 #include <CGAL/Arr_segment_traits_2.h>
 
+typedef int Number_type;
+typedef CGAL::Simple_cartesian<Number_type> Kernel;
+typedef CGAL::Arr_segment_traits_2<Kernel> Traits_2;
+typedef Traits_2::Point_2 Point_2;
+typedef Traits_2::X_monotone_curve_2 Segment_2;
+typedef CGAL::Arrangement_2<Traits_2> Arrangement_2;
+typedef Arrangement_2::Vertex_handle Vertex_handle;
+typedef Arrangement_2::Halfedge_handle Halfedge_handle;
+
 typedef std::vector<int> Vars_List;
 typedef std::pair<int, int> Point_raw;
 typedef std::pair<Point_raw, Point_raw> Edge_raw;
-
 // die erste Vars_list ist für die Finale Zuordnung, in dem zweiten Vector können zwischenzustände gespeichert werden
 std::pair<Vars_List, std::vector<Vars_List>> cadical_wrapper(int number_vars,
                                                              int number_edges_vars,
