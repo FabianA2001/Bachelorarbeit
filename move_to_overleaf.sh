3e15beac3bd493ee409f4be82f93b4fc1639e3f7
Fabian <>
2025-08-11 13:29:07 +0200

    rekursive

--- move_to_overleaf.sh
+++ move_to_overleaf.sh
@@ -6,7 +6,7 @@ ZIEL="/Users/fabian/uni/Bachelorarbeit/682487b555c0d63c84fc8118"
 
 cp "$QUELLE/thesis.tex" $ZIEL
 cp "$QUELLE/chapters"/*.tex "$ZIEL/chapters"
-cp "$QUELLE/figures"/* "$ZIEL/figures"
+cp -r "$QUELLE/figures"/* "$ZIEL/figures"
 cp "$QUELLE/bibliography.bib" "$ZIEL/bibliography.bib"
 
 echo "Alle .tex-Dateien wurden von $QUELLE nach $ZIEL kopiert."
