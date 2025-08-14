48f2ea70e9319234ed7a5912fc2ebc585d094609
Fabian <>
2025-08-14 15:58:17 +0200

    update eval11

--- code/evaluation/eval#11/eval.py
+++ code/evaluation/eval#11/eval.py
@@ -8,7 +8,7 @@ from dc_triangulation import (
     Run_Algbench,
 )
 
-TIMEOUT = 40
+TIMEOUT = 300
 path = os.path.join(os.path.dirname(__file__), "instances")
 # This is the entry point for the evaluation script
 # It will run the Run_Instance class from run_algbench module
@@ -52,7 +52,7 @@ RI = Run_Algbench(
 
 @slurminade.slurmify()
 def run_solver_on_inst(key: str):
-    RI.add_entrys(key, 2)
+    RI.add_entrys(key)
 
 
 @slurminade.slurmify(mail_type="ALL")
