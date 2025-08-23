5abb513a43cd9f3f7b503e67114603a8bb1fe684
“Fabian <farbkecks@gmx.de>
2025-08-23 20:57:46 +0200

    update eval15

--- code/copy.sh
+++ code/copy.sh
@@ -1,6 +1,6 @@
 cd ./run_dct/eval_instance
 
-find . -type f -name '*_180.json*' -exec sh -c '
+find . -type f -name '*_200.json*' -exec sh -c '
   for f; do
     target="../../evaluation/eval#15/instances/${f#./}"  # relative Pfad ohne ./ 
     mkdir -p "$(dirname "$target")"                      # Zielordner erstellen
